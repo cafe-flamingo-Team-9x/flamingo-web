f592f0afdd8212364aa3fa61e9ba5286350ba3de
Savindu Amalka <savinduamalka@gmail.com>
2025-11-13 00:19:39 +0530

    feat: add S3 upload and delete functionality for menu images

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,16 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   experimental: {
     authInterrupts: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lywrplxajfgqnauwdphc.supabase.co',
+      },
+    ],
   },
 };
 
